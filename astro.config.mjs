c56680cb01e58f772aa2837c35df631a17b5a4ac
Federico Mohnen <federicomohnen@gmail.com>
2026-05-05 21:34:09 -0300

    Add decap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap()
+	],
 	fonts: [
 		{
 			provider: fontProviders.local(),
